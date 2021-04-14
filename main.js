48b648d6bdc65cf59e33976efa3e1690c84679c1
Andrew Eyhorn <andrew.eyhorn@gmail.com>
2021-04-14 19:13:56 +1000

    added achievement unlock notification. Game currently has a memory leak issue caused by the formatting library

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ function calculateOfflineProgress() {
     diff = Date.now() - gameData.lastTick;
     gameData.lastTick = Date.now()
     gameData.currency += gameData.speed * (diff / 1000)
-    document.getElementById("currencyVisual").textContent = Math.floor(gameData.currency) + " Currency"
+    document.getElementById("currencyVisual").firstChild.data = Math.floor(gameData.currency) + " Currency"
     gameData.energy += totalEnergyProduction() * (diff / 1000)
-    document.getElementById("energyVisual").textContent = Math.floor(gameData.energy) + " miliJoules of energy"
+    document.getElementById("energyVisual").firstChild.data = Math.floor(gameData.energy) + " miliJoules of energy"
 }
 function totalEnergyProduction() {
     totalOutput = 0
@@ -40,7 +40,7 @@ function totalEnergyProduction() {
 }
 function giveCurrency() {
     gameData.currency += gameData.currencyPerClick
-    document.getElementById("currencyVisual").textContent = Math.floor(gameData.currency) + " Currency"
+    document.getElementById("currencyVisual").firstChild.data = Math.floor(gameData.currency) + " Currency"
 };
 function increaseSpeed() {
     let x = document.getElementById("increaseSpeedSelector")
@@ -50,9 +50,9 @@ function increaseSpeed() {
         gameData.energy -= energyNeeded
         gameData.speed += incrementAmount
         energyNeeded = Math.round(gameData.mass / 2000 * ((incrementAmount/1000) ** 2 + 2 * (incrementAmount/1000) * gameData.speed/1000))
-        document.getElementById("increaseSpeed").textContent = "for " + energyNeeded + " miliJoules of energy"
-        document.getElementById("energyVisual").textContent = Math.floor(gameData.energy) + " miliJoules of energy"
-        document.getElementById("speedVisual").textContent = Math.floor(gameData.speed) + " mm/s"
+        document.getElementById("increaseSpeed").firstChild.data = "for " + energyNeeded + " miliJoules of energy"
+        document.getElementById("energyVisual").firstChild.data = Math.floor(gameData.energy) + " miliJoules of energy"
+        document.getElementById("speedVisual").firstChild.data = Math.floor(gameData.speed) + " mm/s"
     }
 }
 function navigate(menu) {
@@ -71,6 +71,7 @@ function checkAchievements() {
         if (gameData[currentAchievement.property] >= currentAchievement.value) {
             gameData.achievements[i].unlocked = true
             currentAchievement.reward()
+            notification("Achievement Unlocked: " + currentAchievement.name)
         }
 
     }
@@ -167,16 +168,16 @@ function buyBuilding(building) {
         priceAndAmount = calculateBuildingPrice(building)
         gameData.currency -= priceAndAmount[0]
         building.amountOwned += priceAndAmount[1]
-        document.getElementById(building.id).textContent = "Buy " + calculateBuildingPrice(building)[1]  + building.name + " for " + calculateBuildingPrice(building)[0] + " currency"
-        document.getElementById("currencyVisual").textContent = Math.floor(gameData.currency) + " Currency"
-        document.getElementById("energyProductionVisual").textContent = Math.floor(totalEnergyProduction()) + " mJ/s"
+        document.getElementById(building.id).firstChild.data = "Buy " + calculateBuildingPrice(building)[1]  + building.name + " for " + calculateBuildingPrice(building)[0] + " currency"
+        document.getElementById("currencyVisual").firstChild.data = Math.floor(gameData.currency) + " Currency"
+        document.getElementById("energyProductionVisual").firstChild.data = Math.floor(totalEnergyProduction()) + " mJ/s"
     } else {
         if (gameData.currency >= calculateBuildingPrice(building)) {
             gameData.currency -= calculateBuildingPrice(building)
             building.amountOwned += gameData.bulkBuy
-            document.getElementById(building.id).textContent = "Buy " + gameData.bulkBuy + building.name + " for " + calculateBuildingPrice(building) + " currency"
-            document.getElementById("currencyVisual").textContent = Math.floor(gameData.currency) + " Currency"
-            document.getElementById("energyProductionVisual").textContent = Math.floor(totalEnergyProduction()) + " mJ/s"
+            document.getElementById(building.id).firstChild.data = "Buy " + gameData.bulkBuy + building.name + " for " + calculateBuildingPrice(building) + " currency"
+            document.getElementById("currencyVisual").firstChild.data = Math.floor(gameData.currency) + " Currency"
+            document.getElementById("energyProductionVisual").firstChild.data = Math.floor(totalEnergyProduction()) + " mJ/s"
         }
     }
 }
@@ -184,9 +185,9 @@ function showBuildings() {
     let previousPurchased = false
     for (let building of gameData.buildings) {
         if (gameData.bulkBuy === "Max") {
-            document.getElementById(building.id).textContent = "Buy " + calculateBuildingPrice(building)[1]  + building.name + " for " + calculateBuildingPrice(building)[0] + " currency"
+            document.getElementById(building.id).firstChild.data = "Buy " + calculateBuildingPrice(building)[1]  + building.name + " for " + calculateBuildingPrice(building)[0] + " currency"
         } else {
-            document.getElementById(building.id).textContent = "Buy " + gameData.bulkBuy + building.name + " for " + calculateBuildingPrice(building) + " currency"
+            document.getElementById(building.id).firstChild.data = "Buy " + gameData.bulkBuy + building.name + " for " + calculateBuildingPrice(building) + " currency"
             }
         const instance = tippy(document.getElementById(building.id))
         instance.setContent("You have " + building.amountOwned + " " + building.name + " producing " + building.amountOwned*building.production + "mj/s")
@@ -200,12 +201,38 @@ function showBuildings() {
         }
     }
 }
+async function notification(text) {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    let op = 0.1;  
+    const element = document.getElementById("popupNotification")
+    element.innerText = text
+    element.style.opacity = 0
+    element.style.display = 'flex';
+    let timer1 = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer1);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+    await delay(5000)
+    let timer2 = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer2);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 100);
+}
 document.addEventListener('input', function (event) {
 	if (event.target.id !== 'increaseSpeedSelector') return;
     let x = document.getElementById("increaseSpeedSelector")
     let incrementAmount = parseInt(x.options[x.selectedIndex].value)
     let energyNeeded = Math.round(gameData.mass / 2000 * ((incrementAmount/1000) ** 2 + 2 * (incrementAmount/1000) * gameData.speed/1000))
-    document.getElementById("increaseSpeed").textContent = "for " + numberformat.format(energyNeeded, {format: 'standard'}) + " miliJoules of energy"
+    document.getElementById("increaseSpeed").firstChild.data = "for " + numberformat.format(energyNeeded, {format: 'standard'}) + " miliJoules of energy"
 }, false);
 //mainGameLoop updates gameData and visuals
 let mainGameLoop = null
@@ -213,22 +240,23 @@ function gameCalcluations() {
     mainGameLoop = window.setInterval(function () {
         if (gameData.autobuyers.accelerate[0] === true) { increaseSpeed(); }
         if (document.getElementsByClassName("menu")[1].style.display === 'inline-block') { showBuildings(); }
-        checkAchievements()
         gameData.currency += gameData.speed / (1000 / gameData.settings.tickSpeed)
         gameData.energy += totalEnergyProduction() / (1000 / gameData.settings.tickSpeed)
-        document.getElementById("currencyVisual").textContent = numberformat.format(gameData.currency, {format: 'standard'}) + " Currency"
-        document.getElementById("energyVisual").textContent = numberformat.format(gameData.energy, {format: 'standard'}) + " miliJoules of energy"
+        // document.getElementById("currencyVisual").firstChild.data = numberformat.format(gameData.currency, {format: 'standard'}) + " Currency"
+        // document.getElementById("energyVisual").firstChild.data = numberformat.format(gameData.energy, {format: 'standard'}) + " miliJoules of energy"
+        document.getElementById("currencyVisual").firstChild.data = gameData.currency+ " Currency"
+        document.getElementById("energyVisual").firstChild.data = gameData.energy + " miliJoules of energy"
     }, gameData.settings.tickSpeed)
 }
 function stopGameCalculations() {
     clearInterval(mainGameLoop)
-}
+}   
 let tickSpeedSliderValues = [50, 100, 125, 250, 500, 1000]
 let slider = document.getElementById("tickSpeedSlider");
 let sliderOutput = document.getElementById("tickSpeedSliderOutput")
-sliderOutput.textContent = "Tickspeed: " + tickSpeedSliderValues[slider.value] + " ms/tick"
+sliderOutput.firstChild.data = "Tickspeed: " + tickSpeedSliderValues[slider.value] + " ms/tick"
 slider.oninput = function () {
-    sliderOutput.textContent = tickSpeedSliderValues[slider.value]
+    sliderOutput.firstChild.data = tickSpeedSliderValues[slider.value]
     gameData.settings.tickSpeed = tickSpeedSliderValues[slider.value]
     stopGameCalculations()
     gameCalcluations()
